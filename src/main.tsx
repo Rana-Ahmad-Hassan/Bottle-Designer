5149d49f5e150ea3dc221f63dace72195cfbf693
Rana-Ahmad-Hassan <r80228927@gmail.com>
2025-03-08 21:46:29 +0500

    clean the code

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeChooserProvider } from './contexts/theme-chooser.tsx';
 
 // Create a client
 export const queryClient = new QueryClient();
@@ -12,11 +11,9 @@ export const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeChooserProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </ThemeChooserProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
